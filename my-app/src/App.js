2407a476d69369997bdd6eed302c8173faee9ead
lOstinsOul <99341814+lOstinsOul@users.noreply.github.com>
2023-02-16 19:27:00 +0530

    Initial files added

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './app.scss';
+import Services from './components/services/Services';
+
+function App() {
+  return (
+    <div className="App">
+<Services/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/services/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './services.scss'
+const Services = () => {
+  return (
+    <div className="background">
+    <h1>Services</h1>
+    </div>
+  )
+}
+
+export default Services
